c6ec84dd8194f2bfc991f619dd3bdf947e5fac4a
khushbu <khushburajak17@gmail.com>
2025-08-02 09:48:27 +0545

    Add data encryption utilities

---
+++ server/utils/encryptionUtils.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto');
+
+exports.encryptData = (data, key) => {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    let encrypted = cipher.update(data, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return { iv: iv.toString('hex'), encryptedData: encrypted };
+};
+
+exports.decryptData = (encryptedData, key, iv) => {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.from(iv, 'hex'));
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+};
